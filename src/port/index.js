903ae21e28602df25c99c6ee6f2df6cf4ad46250
Putra Nugroho <putraa.nugroho@gmail.com>
2022-03-24 10:27:10 +0700

    Update index.js

--- src/port/index.js
+++ src/port/index.js
@@ -1,3 +1,3 @@
-let port = process.env.PORT || 'https://project-hris-db.herokuapp.com/' || "http://localhost:4000"
+let port = process.env.PORT || 'https://project-hris-db.herokuapp.com' || "http://localhost:4000"
 
 module.exports = port
